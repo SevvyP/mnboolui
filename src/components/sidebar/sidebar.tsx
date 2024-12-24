6289cb9378689931d877a7a97dfe6642f9b5bd6b
SevvyP <longisaac06@gmail.com>
2024-12-23 22:08:42 -0500

    fix sidebar transition

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -6,7 +6,7 @@ export default function Sidebar() {
   const { isSidebarOpen } = useSidebar();
 
   return (
-    <div className={`flex-col w-64 bg-white border-r border-gray-200 transform transition-transform duration-300 ease-in-out ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full -ml-64'}`}>
+    <div className={`flex-col w-64 bg-gray-50 transform duration-300 ease-in-out ${isSidebarOpen ? 'ml-0' : '-ml-64'}`}>
       <h1>Home</h1>
       <h1>Profile</h1>
       <h1>Settings</h1>
