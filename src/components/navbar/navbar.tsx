d40eb9b4ea4b98a0d432a4b32766eddd1d2d71a1
SevvyP <longisaac06@gmail.com>
2025-01-02 19:49:05 -0600

    cleaning up stlyes

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,26 +1,38 @@
-'use client'
+"use client";
 
-import Racoon from '@/app/icon.png';
-import Image from 'next/image';
-import UserProfile from '@/components/userprofile/userprofile';
-import { useSidebar } from '@/contexts/sidebarcontext';
+import Racoon from "@/app/icon.png";
+import Image from "next/image";
+import UserProfile from "@/components/userprofile/userprofile";
+import { useSidebar } from "@/contexts/sidebarcontext";
 
 export default function Navbar() {
+  const { toggleSidebar } = useSidebar();
 
-    const { toggleSidebar } = useSidebar();
-
-    return (
-        <div className="flex items-center h-14 w-full bg-white border-b bg-gray-50 border-gray-200 dark:bg-gray-800 dark:border-gray-700">
-            <div className='ml-2 pointer-events-auto hover:bg-slate-200 dark:hover:bg-gray-700' onClick={toggleSidebar}>
-                <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                    <path clipRule="evenodd" fillRule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
-                </svg>
-            </div>
-            <Image src={Racoon} alt="Racoon" width={50} height={50} />
-            <h1 className='text-xl font-semibold'>mnbool</h1>
-            <div className='mx-auto mr-10'>
-                <UserProfile />
-            </div>
-        </div>
-    )
+  return (
+    <div className="flex items-center h-14 w-full bg-white border-b bg-gray-50 border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+      <div
+        className="ml-2 pointer-events-auto hover:bg-slate-200 dark:hover:bg-gray-700"
+        onClick={toggleSidebar}
+      >
+        <svg
+          className="w-6 h-6 cursor-pointer"
+          aria-hidden="true"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            clipRule="evenodd"
+            fillRule="evenodd"
+            d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
+          ></path>
+        </svg>
+      </div>
+      <Image src={Racoon} alt="Racoon" width={50} height={50} />
+      <h1 className="text-xl font-semibold">mnbool</h1>
+      <div className="mx-auto mr-10">
+        <UserProfile />
+      </div>
+    </div>
+  );
 }
--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,35 +1,48 @@
-'use client'
+"use client";
 
-import { useSidebar } from '@/contexts/sidebarcontext';
+import { useSidebar } from "@/contexts/sidebarcontext";
 
-const Items = [{
-  name: 'Home',
-  link: '/',
-}, {
-  name: 'Events',
-  link: '/events',
-},
-{
-  name: 'Groups',
-  link: '/groups',
-}, {
-  name: 'Settings',
-  link: '/settings',
-}]
+const Items = [
+  {
+    name: "Home",
+    link: "/",
+  },
+  {
+    name: "Events",
+    link: "/events",
+  },
+  {
+    name: "Groups",
+    link: "/groups",
+  },
+  {
+    name: "Settings",
+    link: "/settings",
+  },
+];
 
 export default function Sidebar() {
   const { isSidebarOpen } = useSidebar();
 
   return (
-    <div className={`flex-col w-64 shrink-0 bg-gray-50 dark:bg-gray-800 transform duration-300 ease-in-out ${isSidebarOpen ? 'ml-0' : '-ml-64'}`}>
-      <div className='h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800 '>
-        <div className='space-y-2 font-medium'>
+    <div
+      className={`flex-col w-64 shrink-0 bg-gray-50 dark:bg-gray-800 transform duration-300 ease-in-out ${
+        isSidebarOpen ? "ml-0" : "-ml-64"
+      }`}
+    >
+      <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800 ">
+        <div className="space-y-2 font-medium">
           {Items.map((item, index) => (
-            <h1 key={index} className='flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group dark:text-white dark:hover:bg-gray-700'
-              onClick={() => window.location.href = item.link}>{item.name}</h1>
+            <h1
+              key={index}
+              className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group dark:text-white dark:hover:bg-gray-700 select-none cursor-pointer"
+              onClick={() => (window.location.href = item.link)}
+            >
+              {item.name}
+            </h1>
           ))}
         </div>
       </div>
     </div>
-  )
+  );
 }
